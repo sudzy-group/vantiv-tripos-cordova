043334a59820909e250e23ef169c0570044b7b5d
Roy Ganor <ganoro@gmail.com>
2018-03-23 13:22:31 +0300

    vanity updated api

--- src/ios/TriposCordova.m
+++ src/ios/TriposCordova.m
@@ -11,14 +11,20 @@
     NSOperationQueue* _queue;
     VTPConfiguration* _vtpConfiguration;
     VTP* _sharedVtp;
+
+    VXP* _sharedVxp;
+    VXPCredentials* _sharedCredentials;
+    VXPApplication* _sharedApplication;
 }
 
 - (void)init:(CDVInvokedUrlCommand*)command;
 - (void)sale:(CDVInvokedUrlCommand*)command;
 - (void)refund:(CDVInvokedUrlCommand*)command;
+- (void)creditcardReturn:(CDVInvokedUrlCommand*)command;
 - (void)sendJsonFromDictionary:(NSDictionary*)dictionary;
 - (void)handleNotInit;
 - (NSString *) getTransactionStatusName:(VTPTransactionStatus)transactionStatus;
+- (NSString *) getPaymentTypeName:(VTPPaymentType)paymentType;
 @end
 
 @implementation TriposCordova
@@ -52,6 +58,13 @@
     _vtpConfiguration.deviceConfiguration.isContactlessMsdEntryAllowed = YES;
     _vtpConfiguration.deviceConfiguration.terminalId = @"00000000";
     
+    NSString *tcpIpAddress = [command.arguments objectAtIndex:6];
+    if (tcpIpAddress != nil) {
+        _vtpConfiguration.deviceConfiguration.tcpIpConfiguration = [[VTPDeviceTcpIpConfiguration alloc] init];
+        _vtpConfiguration.deviceConfiguration.tcpIpConfiguration.ipAddress = tcpIpAddress;
+        _vtpConfiguration.deviceConfiguration.tcpIpConfiguration.port = 12000;
+    }
+    
     _vtpConfiguration.hostConfiguration.acceptorId = [command.arguments objectAtIndex:1];
     _vtpConfiguration.hostConfiguration.accountId = [command.arguments objectAtIndex:2];
     _vtpConfiguration.hostConfiguration.accountToken = [command.arguments objectAtIndex:3];
@@ -70,7 +83,7 @@
     _vtpConfiguration.transactionConfiguration.isEmvAllowed = YES;
     
     NSLog(@"init 3 called from %@!", [self class]);
-
+    
     NSError* error;
     if (![_sharedVtp inititializeWithConfiguration:_vtpConfiguration error:&error])
     {
@@ -84,26 +97,26 @@
     
     VTPHostConfiguration* hostConfiguration = _vtpConfiguration.hostConfiguration;
     
-    VXPCredentials* credentials = [VXPCredentials
+    _sharedCredentials = [VXPCredentials
                                    credentialsWithValues:hostConfiguration.accountId
                                    accountToken:hostConfiguration.accountToken
                                    acceptorID:hostConfiguration.acceptorId];
     
-    VXPApplication* application = [VXPApplication
+    _sharedApplication = [VXPApplication
                                    applicationWithValues:hostConfiguration.applicationId
                                    applicationName:hostConfiguration.applicationName
                                    applicationVersion:hostConfiguration.applicationVersion];
     
     VXPRequest* healthCheckRequest = [VXPRequest requestWithRequestType:VXPRequestTypeHealthCheck
-                                                            credentials:credentials
-                                                            application:application];
+                                                            credentials:_sharedCredentials
+                                                            application:_sharedApplication];
     
     NSLog(@"init 4 called from %@!", [self class]);
     
-    VXP* vxp = [[VXP alloc] init];
-    vxp.TestCertification = _vtpConfiguration.applicationConfiguration.mode == VTPApplicationModeTestCertification;
+    _sharedVxp = [[VXP alloc] init];
+    _sharedVxp.TestCertification = _vtpConfiguration.applicationConfiguration.mode == VTPApplicationModeTestCertification;
     
-    [vxp sendRequest:healthCheckRequest
+    [_sharedVxp sendRequest:healthCheckRequest
              timeout: 10 * 1000
         autoReversal:YES
    completionHandler:^(VXPResponse* response)
@@ -170,8 +183,88 @@
 
 - (void)refund:(CDVInvokedUrlCommand*)command
 {
+    _callbackId = command.callbackId;
     
+    VTPRefundRequest* request = [[VTPRefundRequest alloc] init];
+    
+    request.cardholderPresentCode = VTPCardHolderPresentCodePresent;
+    request.clerkNumber = @"123";
+    request.convenienceFeeAmount = nil;
+    request.laneNumber = @"1";
+    request.referenceNumber = [command.arguments objectAtIndex:0];
+    request.salesTaxAmount = nil;
+    request.shiftID = @"2";
+    request.ticketNumber = [command.arguments objectAtIndex:1];
+    
+    NSString *amount = [command.arguments objectAtIndex:2];
+    NSString *transactionAmount = [amount stringByTrimmingCharactersInSet:[NSCharacterSet symbolCharacterSet]];
+    request.transactionAmount = [NSDecimalNumber decimalNumberWithString:transactionAmount];
+    
+    _sharedVtp = triPOSMobileSDK.sharedVtp;
+    
+    if (_sharedVtp.isInitialized)
+    {
+        [_sharedVtp processRefundRequest :request
+                     completionHandler:^(VTPRefundResponse* response)
+         {
+             [self refundRequestComplete:response];
+         }
+          errorHandler:^(NSError* error)
+         {
+             [self handleError:error];
+         }];
+    }
+    else
+    {
+        [self handleNotInit];
+    }
 }
+
+
+- (void)creditcardReturn:(CDVInvokedUrlCommand*)command
+{
+    _callbackId = command.callbackId;
+    
+    VXPTransaction *transaction = [[VXPTransaction alloc] init];
+    
+    transaction.TransactionID = [command.arguments objectAtIndex:0];
+
+    transaction.ReferenceNumber = [command.arguments objectAtIndex:1];
+
+    NSString *amount = [command.arguments objectAtIndex:2];
+    NSString *transactionAmount = [amount stringByTrimmingCharactersInSet:[NSCharacterSet symbolCharacterSet]];
+    transaction.TransactionAmount = [NSDecimalNumber decimalNumberWithString:transactionAmount];
+    
+    VXPTerminal *terminal = [[VXPTerminal alloc] init];
+    terminal.TerminalID = @"1";
+    terminal.CardPresentCode = VXPCardPresentCodePresent;
+    terminal.CardholderPresentCode = VXPCardHolderPresentCodePresent;
+    terminal.CVVPresenceCode = VXPCVVPresenceCodeDefault;
+    terminal.TerminalCapabilityCode = VXPTerminalCapabilityCodeDefault;
+    terminal.TerminalEnvironmentCode = VXPTerminalEnvironmentCodeDefault;
+    terminal.MotoECICode = VXPMotoECICodeNotUsed;
+    terminal.CardInputCode = VXPCardInputCodeMagstripeRead;
+    
+    VXPRequest* creditCardReturnRequest = [VXPRequest requestWithRequestType:VXPRequestTypeCreditCardReturn
+                         credentials:_sharedCredentials
+                         application:_sharedApplication];
+    creditCardReturnRequest.Transaction = transaction;
+    creditCardReturnRequest.Terminal = terminal;
+    
+    [_sharedVxp sendRequest:creditCardReturnRequest
+             timeout:10000
+        autoReversal:YES
+   completionHandler:^(VXPResponse* response)
+     {
+         [self handleReturnResponse:response];
+     }
+        errorHandler:^(NSError* error)
+     {
+         [self handleError:error];
+     }];
+}
+
+
 
 -(void)sendJsonFromDictionary:(NSDictionary*)dictionary {
     NSError* error;
@@ -206,6 +299,42 @@
     [self sendJsonFromDictionary:responseArray];
 }
 
+
+- (void)refundRequestComplete:(VTPRefundResponse*)response
+{
+
+    NSString* transactionStatus = [self getTransactionStatusName:response.transactionStatus];
+    
+    NSString* paymentType = [self getPaymentTypeName:response.paymentType];
+    
+    NSDateFormatter* dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
+    NSString* dateString = [dateFormatter stringFromDate:response.transactionDateTime];
+    
+    NSString *transactionId = response.host.transactionID;
+    
+    NSDictionary *responseArray = [NSDictionary dictionaryWithObjectsAndKeys:
+                                   transactionStatus, @"transactionStatus",
+                                   paymentType, @"paymentType",
+                                    [NSString stringWithFormat:@"%@", response.referenceNumber], @"referenceNumber",
+                                   transactionId ? transactionId : @"<nil>", @"transactionId",
+                                   dateString ? dateString : @"<nil>", @"transactionDateTime", nil];
+    
+    [self sendJsonFromDictionary:responseArray];
+}
+
+- (void)handleReturnResponse:(VXPResponse*) response
+{
+    NSDictionary *responseArray = [NSDictionary dictionaryWithObjectsAndKeys:
+                                   response.Transaction.TransactionStatus, @"transactionStatus",
+                                   response.Transaction.TransactionID, @"transactionId",
+                                   response.Transaction.ApprovedAmount, @"approvedAmount",
+                                   response.Transaction.ReferenceNumber, @"referenceNumber", nil];
+    
+    [self sendJsonFromDictionary:responseArray];
+}
+
+
 - (void)handleError:(NSError*)error
 {
     NSDictionary *responseArray = [NSDictionary dictionaryWithObjectsAndKeys:
@@ -222,7 +351,6 @@
     [self sendJsonFromDictionary:responseArray];
 }
 
-
 - (NSString *) getTransactionStatusName:(VTPTransactionStatus)transactionStatus
 {
     switch (transactionStatus)
@@ -234,6 +362,20 @@
         case VTPTransactionStatusNeedsToBeReversed:
             return @"Needs to be Reversed";
         case VTPTransactionStatusUnknown:
+        default:
+            return @"Unknown";
+    }
+}
+
+- (NSString *) getPaymentTypeName:(VTPPaymentType)paymentType
+{
+    switch (paymentType)
+    {
+        case VTPPaymentTypeCredit:
+            return @"Credit";
+        case VTPPaymentTypeDebit:
+            return @"Debit";
+        case VTPPaymentTypeUnknown:
         default:
             return @"Unknown";
     }
--- www/TriposCordova.js
+++ www/TriposCordova.js
@@ -1,10 +1,17 @@
 var exec = require('cordova/exec');
 
-exports.init = function (mode, acceptorId, accountId, accountToken, applicationId, deviceType, success, error) {
-    exec(success, error, 'TriposCordova', 'init', [mode, acceptorId, accountId, accountToken, applicationId, deviceType]);
+exports.init = function (mode, acceptorId, accountId, accountToken, applicationId, deviceType, deviceAddress, success, error) {
+    exec(success, error, 'TriposCordova', 'init', [mode, acceptorId, accountId, accountToken, applicationId, deviceType, deviceAddress]);
 };
 
 exports.sale = function (refNum, ticketNumber, transactionAmount, success, error) {
     exec(success, error, 'TriposCordova', 'sale', [refNum, ticketNumber, transactionAmount]);
 };
 
+exports.refund = function (referenceNumber, ticketNumber, transactionAmount, success, error) {
+    exec(success, error, 'TriposCordova', 'refund', [referenceNumber, ticketNumber, transactionAmount]);
+};
+
+exports.creditCardReturn = function (transactionId, referenceNumber, transactionAmount, success, error) {
+    exec(success, error, 'TriposCordova', 'creditCardReturn', [transactionId, referenceNumber, transactionAmount]);
+};
